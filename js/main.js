c97f5982078ecedf7e289a9b1e8f1a523a74457e
JuliaGrid <julia.grid2000@gmail.com>
2019-07-04 19:31:19 +0300

    Изменения после 1 защиты, завершение
    Код выровнен, подключены минифицированные файлы

--- js/main.js
+++ js/main.js
@@ -7,38 +7,38 @@ var adults = modalSearching.querySelector('[name=adults]');
 var children = modalSearching.querySelector('[name=children]');
 var form = modalSearching.querySelector('.data-of-hotel');
 
-var onButtonSearchingClickOpen = function(){
-   modalSearching.classList.remove('hidden');
+var onButtonSearchingClickOpen = function() {
+  modalSearching.classList.remove('hidden');
 }
-var onButtonSearchingClickCLose = function(){
-   modalSearching.classList.add('hidden');
-   modalSearching.classList.remove("modal-eror");
+var onButtonSearchingClickCLose = function() {
+  modalSearching.classList.add('hidden');
+  modalSearching.classList.remove("modal-eror");
 }
 
 form.addEventListener('submit', function(evt) {
-   modalSearching.classList.remove("modal-eror");
+  modalSearching.classList.remove("modal-eror");
   if (!race.value || !departure.value || !adults.value || !children.value) {
     evt.preventDefault();
     modalSearching.classList.remove("modal-eror");
     modalSearching.offsetWidth = modalSearching.offsetWidth;
     modalSearching.classList.add("modal-eror");
-    if(!race.value){
+    if (!race.value) {
       race.focus();
-    }else if(!departure.value){
+    } else if (!departure.value) {
       departure.focus();
-    }else if(!adults.value){
+    } else if (!adults.value) {
       adults.focus();
-    }else if(!children.value){
+    } else if (!children.value) {
       children.focus();
     }
   }
 });
 
-buttonSearching.addEventListener('click', function(evt){
+buttonSearching.addEventListener('click', function(evt) {
   evt.preventDefault();
-  if(modalSearching.classList.contains('hidden')) {
+  if (modalSearching.classList.contains('hidden')) {
     onButtonSearchingClickOpen();
-  }else {
+  } else {
     onButtonSearchingClickCLose();
   }
 });
